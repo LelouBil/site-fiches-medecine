1bb2df356dbef11a581adfe72974f02316c8933a
Bilel MEDIMEGH <bilel.medimegh@gmail.com>
2024-10-15 01:31:04 +0200

    Changement url

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: "https://leloubil.github.com",
-  base: 'medecine_fiches',
+  base: 'site-fiches-medecine',
   trailingSlash: 'never',
 
   experimental: {
