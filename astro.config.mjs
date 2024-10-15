c88cf9c80e00ca932da40c2e89890c7d9614866e
Bilel MEDIMEGH <bilel.medimegh@gmail.com>
2024-10-15 03:19:50 +0200

    domaine

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://leloubil.github.com",
-  base: 'site-fiches-medecine',
+  site: "https://medhisouk.fr",
+  base: '',
   trailingSlash: 'never',
 
   experimental: {
